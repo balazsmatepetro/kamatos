7a5484218f025f9c63732c04a3c6e8380a13d0de
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-04-23 19:59:24 +0200

    CalculationController added to the DI Container

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Kamatos\Http\Application;
+use Kamatos\Http\Controller;
 use Kamatos\Http\Provider;
 
 $config = require __DIR__ . '/../config.php';
@@ -9,7 +10,13 @@ $config = require __DIR__ . '/../config.php';
 $app = new Application(['settings' => $config]);
 
 $app->bind('logger', new Provider\LoggerProvider($config['logger']['name'], $config['logger']['handler']));
-$app->bind('view', new Provider\TwigProvider($config['twig']['path'], $config['twig']['cache_path']));
+$app->bind('twig', new Provider\TwigProvider($config['twig']['path'], $config['twig']['cache_path']));
+$app->bind(Controller\CalculationController::class, function ($container) {
+    $router = $container->get('router');
+    $twig = $container->get('twig');
+    
+    return new Controller\CalculationController($router, $twig);
+});
 
 require __DIR__ . '/routes.php';
 
